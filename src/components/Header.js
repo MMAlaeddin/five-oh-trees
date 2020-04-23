960ab0a36dc989e877bd56863423d974f3d5481e
Mariam Alaeddin <mariamalaeddin@gmail.com>
2020-04-23 10:31:45 -0700

    fix bugs in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,13 @@
+import React from "react";
+
+function Header(){
+  return (
 <div class="headerFooter">
   <h1>Five-Oh-Trees!</h1>
   <h3>Smokin' Style</h3>
 </div>
+  );
+}
+
+export default Header; 
+
